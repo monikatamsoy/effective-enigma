9519f84ee090a20cd78e952a49d0fe0e31a63ab5
monika-tamsoy <monika.tamsoy@hcl.com>
2022-01-05 00:37:38 +0530

    add onMouseClick and onMouseMove event to change image's position

--- docs/app.js
+++ docs/app.js
@@ -4,12 +4,14 @@ import { LoadingBar } from './libs/LoadingBar.js';
 import { OrbitControls } from './libs/three/jsm/OrbitControls.js';
 
 
+
+
 class App {
     constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
         
-		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 10000 );
 		this.camera.position.set( 0, 0.8, 7 );
         this.camera.lookAt(0,0,0)
         
@@ -33,32 +35,92 @@ class App {
         this.renderer.physicallyCorrectLights = true;
         container.appendChild( this.renderer.domElement );
 		
+        this.mouse = new THREE.Vector2();
+        this.mouse.x = this.mouse.y = -1;
+        this.raycaster = new THREE.Raycaster();
+        this.imageClicked = false;
         this.loadingBar = new LoadingBar();
         this.init();
         
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         this.controls.target.set(0, 0, 0);
         this.controls.update();
+
         
-        // window.addEventListener('resize', this.resize.bind(this) );
-        // document.addEventListener("mousemove", this.onMouseMove, false)
-        // document.addEventListener("click", this.onMouseClick, false)
+        window.addEventListener('resize', this.resize.bind(this) );
+        document.addEventListener("mousemove", this.onMouseMove, false)
+        document.addEventListener("click", this.onMouseClick, false)
 
 	}	
+
+    resize(){
+        
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );  
+    }
+    
     init(){
         const axesHelper = new THREE.AxesHelper( 5 );
         this.scene.add( axesHelper );  
         this.loadCastle();
         this.loadImage();
 
+        
+
     }
+
+    onMouseMove = (e) => {
+            if (this.imageClicked == true )
+            {   
+                this.scene.getObjectByName('image').position.x = this.mouse.x;
+                var vec = new THREE.Vector3(); // create once and reuse
+                var pos = new THREE.Vector3(); // create once and reuse
+                
+                vec.set(
+                    ( e.clientX / window.innerWidth ) * 2 - 1,
+                    - ( e.clientY / window.innerHeight ) * 2 + 1,
+                    0 );
+                    
+                    vec.unproject( this.camera );
+                    
+                    vec.sub( this.camera.position ).normalize();
+                    
+                    var distance = - this.camera.position.z / vec.z;
+                    
+                    pos.copy( this.camera.position ).add( vec.multiplyScalar( distance ) );
+                    this.scene.getObjectByName('image').position.set(pos.x,pos.y,pos.z)
+                    
+            } 
+    } 
+    onMouseClick = (e) => {
+        
+        e.preventDefault();
+        
+        this.mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+        
+        // console.log(mouse)
+        this.raycaster.setFromCamera( this.mouse, this.camera );
+
+        var intersects = this.raycaster.intersectObject( this.scene.getObjectByName('image'));
+        if (intersects[0] )
+        {   
+            this.imageClicked = !this.imageClicked
+        } 
+         
+    
+}
+
 
     loadImage() {
         const map = new THREE.TextureLoader().load( './assets/tower.png' );
         const material = new THREE.SpriteMaterial( { map: map } );
 
-        const sprite = new THREE.Sprite( material );
-        this.scene.add( sprite );
+        this.image = new THREE.Sprite( material );
+        this.image.position.set(5,-1,0)
+        this.image.name = "image"
+        this.scene.add( this.image );
     }
 
     loadCastle(){
@@ -72,7 +134,7 @@ class App {
 			// called when the resource is loaded
 			function ( gltf ) {
                 const bbox = new THREE.Box3().setFromObject( gltf.scene );
-                console.log(`min:${bbox.min.x.toFixed(2)},${bbox.min.y.toFixed(2)},${bbox.min.z.toFixed(2)} -  max:${bbox.max.x.toFixed(2)},${bbox.max.y.toFixed(2)},${bbox.max.z.toFixed(2)}`);
+                // console.log(`min:${bbox.min.x.toFixed(2)},${bbox.min.y.toFixed(2)},${bbox.min.z.toFixed(2)} -  max:${bbox.max.x.toFixed(2)},${bbox.max.y.toFixed(2)},${bbox.max.z.toFixed(2)}`);
                 
                 // gltf.scene.traverse( ( child ) => {
                 //     if (child.isMesh){
